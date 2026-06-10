a56afbc2fa82fa64522d09f52ee35d9a6d060ed3
Anthony0486 <anthony.pruniere@wanadoo.fr>
2026-06-10 11:55:48 +0200

    + v-for

--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -14,7 +14,7 @@ console.log(filmName.value)
 <div class="p-8 pt-8 flex flex-col  items-center justify-center gap-4 counter-card">
 <h1 class="p-8 text-3xl font-bold md:text-4xl">WatchList &#127916</h1>
 <p v-if="filmList.length === 0" class="py-4"><strong>Vous n'avez pas encore de film dans votre liste</strong></p>
-<h2 v-else >{{ filmList }}</h2>
+<p v-else v-for="film in filmList" >{{ film }}</p>
 <input type="text" v-model="filmName" placeholder="Tappez le nom du film à ajouter" class="input input-bordered w-full max-w-xs" />
 <button @click="addFilm(filmName)" class="btn btn-primary">Ajouter un film</button>
 
